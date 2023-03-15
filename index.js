affb2a970f97b3f327e7feb130d11e1e30e484f2
kuldeep-kushwah <kuldeep_k@mt.iitr.ac.in>
2023-03-15 21:35:21 +0530

    changes1

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const http=require("http");
+const fs=require('fs');
+const input=process.argv;
 
+if(input[2]=='add')
+{
+    fs.writeFileSync(input[3],input[4]);
+}
+else if(input[2]=='remove')
+{
+    fs.unlinkSync(input[3]);
+}
+else{
+    console.log('invalid input!')
+}
 
 console.log("server ruuning on port 5000 ...");
 
