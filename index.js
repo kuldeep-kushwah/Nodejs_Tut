55ae2e703c01de43296f21244c4ce18a48362e41
kuldeep-kushwah <kuldeep_k@mt.iitr.ac.in>
2023-03-15 21:46:04 +0530

    update with changes

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const http=require("http");
 const fs=require('fs');
-const data=require('./data');
 const input=process.argv;
 
 if(input[2]=='add')
 {
-   fs.writeFileSync(input[3],input[4]);
-   console.log("file has been created!")
+    fs.writeFileSync(input[3],input[4]);
 }
 else if(input[2]=='remove')
 {
     fs.unlinkSync(input[3]);
 }
+else{
+    console.log('invalid input!')
+}
 
 console.log("server ruuning on port 5000 ...");
-console.log(input);
 
 
 
 
-// http.createServer((req,res)=>{
-//     res.writeHead(200,{'content-t':'application/json'});
-//     res.write(JSON.stringify(data));
-//    // res.write(JSON.stringify({Name:'kuldeep',email:'kuldeepkushwah529@gmail.com'}));
-//     res.end();
-// }).listen(5000);
+
+http.createServer((req,res)=>{
+    res.writeHead(200,{'content-t':'application/json'});
+    res.write(JSON.stringify(data));
+   // res.write(JSON.stringify({Name:'kuldeep',email:'kuldeepkushwah529@gmail.com'}));
+    res.end();
+}).listen(5000);
