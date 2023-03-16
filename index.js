96c7ab9da06d8d4f3e863e15b2e3f205eb7b1423
kuldeep-kushwah <kuldeep_k@mt.iitr.ac.in>
2023-03-16 23:38:57 +0530

    fs module

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 const http=require("http");
 const fs=require('fs');
-const input=process.argv;
+const path=require('path');
 
-if(input[2]=='add')
-{
-    fs.writeFileSync(input[3],input[4]);
-}
-else if(input[2]=='remove')
-{
-    fs.unlinkSync(input[3]);
-}
-else{
-    console.log('invalid input!')
-}
+const dirpath=path.join(__dirname);
+const filepath=`${dirpath}/apple.txt`;
+
+
+
+ fs.writeFileSync(filepath,"apple file has been created!");
+ fs.readFile(filepath,'utf8',(err,f)=>{
+    console.log("showing file content-> "+f);
+ })
+fs.appendFileSync(filepath," this is append file")
+fs.renameSync(filepath,`${dirpath}/mango.txt`,(err)=>{
+      if(!err)console.log("file has been updated!");
+});
+ fs.unlinkSync(`${dirpath}/mango.txt`)
+
+
+// for(i=0;i<5;i++)
+// {
+//     fs.unlinkSync(dirpath+'/file'+i+'.txt',(err)=>{
+//   if(!err)console.log(`${i} file has been deleted`)
+//     });
+
+// }
+
+// fs.readdir(dirpath,(err,files)=>{
+//       files.forEach((file)=>{
+//        console.log(file);
+//       })
+// })
+
+
+
+// const input=process.argv;
+
+
+// if(input[2]=='add')
+// {
+//     fs.writeFileSync(input[3],input[4]);
+// }
+
+// else if(input[2]=='remove')
+// {
+//     fs.unlinkSync(input[3]);
+// }
+// else{
+//     console.log('invalid input!')
+// }
 
 console.log("server ruuning on port 5000 ...");
 
