f59610a5a4e6d42c7cef4fb8dbef94403b6b5817
kuldeep-kushwah <kuldeep_k@mt.iitr.ac.in>
2023-03-22 23:11:16 +0530

    search-api first commit

--- index.js
+++ index.js
@@ -5,29 +5,18 @@ const app=express();
 const product=require('./product');
 app.use(express.json());
 
-app.post('/create',async(req,res)=>{
-      const data=new product(req.body);
-      const result=await data.save();
-     console.log(result);
-     res.send(result);
-})
 
-app.get('/list',async(req,res)=>{
-     const data=await product.find();
+app.get('/search/:key',async(req,res)=>{
+      
+     let data=await product.find({
+          "$or":[
+               {"name":{$regex:req.params.key}},
+               {"category":{$regex:req.params.key}},
+               
+          ]
+     });
      console.log(data);
      res.send(data);
-})
-
-app.delete('/delete/:_id',async(req,res)=>{
-     const result=await product.deleteOne(req.params);
-     console.log(result);
-     res.send(result);
-})
-
-app.put('/update/:_id',async(req,res)=>{
-     const result=await product.updateOne(req.params,{$set:req.body});
-     console.log(result);
-     res.send(result);
 })
 
 app.listen(5000,()=>{
